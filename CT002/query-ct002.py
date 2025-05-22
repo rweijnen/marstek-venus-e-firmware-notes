f4420b83f6990f23c59890d4e70efcc1b50a640c
Remko Weijnen <r.weijnen@gmail.com>
2025-05-22 09:51:53 +0200

    Update query-ct002.py
    added potential support for non Venus batteries

--- CT002/query-ct002.py
+++ CT002/query-ct002.py
@@ -1,24 +1,26 @@
 # -----------------------------------------------------------------------------
 # ct002.py - Send UDP request to Marstek CT002 device
 #
-# This script constructs and sends a UDP packet to a Marstek CT002 device on
-# port 12345. The payload contains information such as device type, battery MAC,
-# CT MAC, and CT type, with a custom XOR-based checksum appended.
+# This script constructs and sends a UDP packet to a Marstek CT002 or CT003
+#  device on port 12345. The payload contains information such as device type,
+# battery MAC, CT MAC, and CT type, with a custom XOR-based checksum appended.
+#
+# IP Address is IP Address of the CT
 #
 # Device types supported:
-#   - Device type: HMG-50 (5.12kWh battery)
+#   - Device type: HMG-50 (Marstek Venus 5.12)
 #   - CT type    : HME-4 (CT002) or HME-3 (CT003)
 #
 # MAC addresses:
 #   - battery_mac and ct_mac must be provided as 12-character hex strings.
 #   - These can be found on the Device Management page in the Marstek app.
+#   - NOT THE DEVICE MAC AS SEEN NON YOUR NETWORK!
 #
 # The script sends a message using ASCII encoding and follows the protocol:
 #   SOH + STX + <2-digit ASCII length> + "|" + fields... + ETX + <2-digit ASCII checksum>
 #
 # Notes:
-# - This script has been tested with the CT002.
-# - It might also work for CT003, but this is unconfirmed.
+# - This script has been tested with the CT002 and CT003
 #
 # Source: https://github.com/rweijnen/marstek-venus-e-firmware-notes/
 # -----------------------------------------------------------------------------
@@ -44,8 +46,8 @@ def calculate_checksum(data_bytes):
     return xor
 
 def GetCTData(ip, device_type, battery_mac, ct_mac, ct_type):
-    if device_type != 'HMG-50':
-        raise ValueError("Only 'HMG-50' is supported as device_type")
+    if not re.fullmatch(r'HM[ABGK]-\d+', device_type):
+        raise ValueError("Device type must be HMB-X, HMA-X, HMK-X, or HMG-X where X is a number")
     if ct_type not in ('HME-4', 'HME-3'):
         raise ValueError("CT type must be either 'HME-4' or 'HME-3'")
     if not validate_mac(battery_mac):
